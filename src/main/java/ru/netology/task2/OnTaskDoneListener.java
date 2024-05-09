db6435c0caf8e40ef5fe2b5a6c92177ea8c283a0
Natalia Kubiak <nataliia.kubiak@gmail.com>
2024-05-09 22:21:02 +0300

    Add lambda task2

---
+++ src/main/java/ru/netology/task2/OnTaskDoneListener.java
@@ -0,0 +1,6 @@
+package ru.netology.task2;
+
+@FunctionalInterface
+public interface OnTaskDoneListener {
+    void onDone(String result);
+}
---
+++ src/main/java/ru/netology/task2/OnTaskErrorListener.java
@@ -0,0 +1,5 @@
+package ru.netology.task2;
+
+public interface OnTaskErrorListener {
+    void onError(String result);
+}
